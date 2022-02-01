7109736dae35027e74dfabe2c11b96f5e0a8d918
Wengier <g217881111@gmail.com>
2022-02-01 23:15:23 +0800

    Add 404 Page

--- pages/clients/[clientId]/index.js
+++ pages/clients/[clientId]/index.js
@@ -1,7 +1,26 @@
+import { useRouter } from 'next/router'
+
 function ClientProjectsPage() {
+  const router = useRouter()
+
+  console.log(router.query)
+
+  function loadProjectHandler() {
+    router.push('/clients/max/projectA')
+    // OR
+    // router.push({
+    //   pathname: '/clients/[clientId]/[projectId]',
+    //   query: {
+    //     clientId: 'max',
+    //     projectId: 'projectA',
+    //   }
+    // })
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,31 @@
+import Link from 'next/link'
+const clients = [
+  { id: 'max', name: 'Maximilian' },
+  { id: 'manu', name: 'Manuel' },
+  { id: 'wen', name: 'Wangier' },
+]
+
+
 function ClientsPage() {
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map(client => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: '/clients/[id]',
+                query: { id: client.id }
+              }}
+            >
+              {client.name}
+            </Link>
+            {/* OR */}
+            {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
